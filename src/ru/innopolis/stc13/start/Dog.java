631bfd7c7e5666f8e19fa4c86507c9235b33b81b
passingbyreloaded <voronayala@mail.ru>
2018-09-11 13:39:22 +0400

    added running for dog

--- src/ru/innopolis/stc13/start/Dog.java
+++ src/ru/innopolis/stc13/start/Dog.java
@@ -1,8 +1,14 @@
 package ru.innopolis.stc13.start;
 
-public class Dog extends Animal {
+public class Dog extends Animal implements Running {
 
     public Dog(String name, Integer nLegs) {
         super(name, nLegs);
     }
+
+    @Override
+    public void run() {
+        System.out.println("Dog runs");
+    }
+
 }
